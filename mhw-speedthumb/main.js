a9c6f238c66da579babe20572c73aa7e73f721e0
stripedypaper <stripedypaper@hotmail.com>
2019-10-25 23:17:18 -0700

    multi mon

--- mhw-speedthumb/main.js
+++ mhw-speedthumb/main.js
@@ -19,15 +19,27 @@ angular.module('app', [])
         'Bow': 'weapon_icons/bow.png'
     });
     vm.monsters = Object.freeze({
+        'Acidic Glavenus': 'monster_icons/MHWI-Acidic_Glavenus_Icon.png',
         'Ancient Leshen': 'monster_icons/MHW-Ancient_Leshen_Icon.png',
         'Anjanath': 'monster_icons/MHW-Anjanath_Icon.png',
         'Azure Rathalos': 'monster_icons/MHW-Azure_Rathalos_Icon.png',
+        'Banbaro': 'monster_icons/MHWI-Banbaro_Icon.png',
+        'Barioth': 'monster_icons/MHWI-Barioth_Icon.png',
         'Bazelgeuse': 'monster_icons/MHW-Bazelgeuse_Icon.png',
         'Behemoth': 'monster_icons/MHW-Behemoth_Icon.png',
+        'Beotodus': 'monster_icons/MHWI-Beotodus_Icon.png',
         'Black Diablos': 'monster_icons/MHW-Black_Diablos_Icon.png',
+        'Blackveil Vaal Hazak': 'monster_icons/MHWI-Blackveil_Vaal_Hazak_Icon.png',
+        'Brachydios': 'monster_icons/MHWI-Brachydios_Icon.png',
+        'Brute Tigrex': 'monster_icons/MHWI-Brute_Tigrex_Icon.png',
+        'Coral Pukei-Pukei': 'monster_icons/MHWI-Coral_Pukei-Pukei_Icon.png',
         'Deviljho': 'monster_icons/MHW-Deviljho_Icon.png',
         'Diablos': 'monster_icons/MHW-Diablos_Icon.png',
         'Dodogama': 'monster_icons/MHW-Dodogama_Icon.png',
+        'Ebony Odogaron': 'monster_icons/MHWI-Ebony_Odogaron_Icon.png',
+        'Fulgur Anjanath': 'monster_icons/MHWI-Fulgur_Anjanath_Icon.png',
+        'Glavenus': 'monster_icons/MHWI-Glavenus_Icon.png',
+        'Gold Rathian': 'monster_icons/MHWI-Gold_Rathian_Icon.png',
         'Great Girros': 'monster_icons/MHW-Great_Girros_Icon.png',
         'Great Jagras': 'monster_icons/MHW-Great_Jagras_Icon.png',
         'Jyuratodus': 'monster_icons/MHW-Jyuratodus_Icon.png',
@@ -39,37 +51,18 @@ angular.module('app', [])
         'Legiana': 'monster_icons/MHW-Legiana_Icon.png',
         'Leshen': 'monster_icons/MHW-Leshen_Icon.png',
         'Lunastra': 'monster_icons/MHW-Lunastra_Icon.png',
+        'Namielle': 'monster_icons/MHWI-Namielle_Icon.png',
+        'Nargacuga': 'monster_icons/MHWI-Nargacuga_Icon.png',
         'Nergigante': 'monster_icons/MHW-Nergigante_Icon.png',
+        'Nightshade Paolumu': 'monster_icons/MHWI-Nightshade_Paolumu_Icon.png',
         'Odogaron': 'monster_icons/MHW-Odogaron_Icon.png',
         'Paolumu': 'monster_icons/MHW-Paolumu_Icon.png',
         'Pink Rathian': 'monster_icons/MHW-Pink_Rathian_Icon.png',
         'Pukei-Pukei': 'monster_icons/MHW-Pukei-Pukei_Icon.png',
         'Radobaan': 'monster_icons/MHW-Radobaan_Icon.png',
+        'Rajang': 'monster_icons/MHWI-Rajang_Icon.png',
         'Rathalos': 'monster_icons/MHW-Rathalos_Icon.png',
         'Rathian': 'monster_icons/MHW-Rathian_Icon.png',
-        'Teostra': 'monster_icons/MHW-Teostra_Icon.png',
-        'Tobi-Kadachi': 'monster_icons/MHW-Tobi-Kadachi_Icon.png',
-        'Tzitzi-Ya-Ku': 'monster_icons/MHW-Tzitzi-Ya-Ku_Icon.png',
-        'Uragaan': 'monster_icons/MHW-Uragaan_Icon.png',
-        'Vaal Hazak': 'monster_icons/MHW-Vaal_Hazak_Icon.png',
-        'Xeno\'jiiva': 'monster_icons/MHW-Xeno\'jiiva_Icon.png',
-        'Zorah Magdaros': 'monster_icons/MHW-Zorah_Magdaros_Icon.png',
-        'Acidic Glavenus': 'monster_icons/MHWI-Acidic_Glavenus_Icon.png',
-        'Banbaro': 'monster_icons/MHWI-Banbaro_Icon.png',
-        'Barioth': 'monster_icons/MHWI-Barioth_Icon.png',
-        'Beotodus': 'monster_icons/MHWI-Beotodus_Icon.png',
-        'Blackveil Vaal Hazak': 'monster_icons/MHWI-Blackveil_Vaal_Hazak_Icon.png',
-        'Brachydios': 'monster_icons/MHWI-Brachydios_Icon.png',
-        'Brute Tigrex': 'monster_icons/MHWI-Brute_Tigrex_Icon.png',
-        'Coral Pukei-Pukei': 'monster_icons/MHWI-Coral_Pukei-Pukei_Icon.png',
-        'Ebony Odogaron': 'monster_icons/MHWI-Ebony_Odogaron_Icon.png',
-        'Fulgur Anjanath': 'monster_icons/MHWI-Fulgur_Anjanath_Icon.png',
-        'Glavenus': 'monster_icons/MHWI-Glavenus_Icon.png',
-        'Gold Rathian': 'monster_icons/MHWI-Gold_Rathian_Icon.png',
-        'Namielle': 'monster_icons/MHWI-Namielle_Icon.png',
-        'Nargacuga': 'monster_icons/MHWI-Nargacuga_Icon.png',
-        'Nightshade Paolumu': 'monster_icons/MHWI-Nightshade_Paolumu_Icon.png',
-        'Rajang': 'monster_icons/MHWI-Rajang_Icon.png',
         'Ruiner Nergigante': 'monster_icons/MHWI-Ruiner_Nergigante_Icon.png',
         'Savage Deviljho': 'monster_icons/MHWI-Savage_Deviljho_Icon.png',
         'Scarred Yian Garuga': 'monster_icons/MHWI-Scarred_Yian_Garuga_Icon.png',
@@ -77,11 +70,18 @@ angular.module('app', [])
         'Shara Ishvalda': 'monster_icons/MHWI-Shara_Ishvalda_Icon.png',
         'Shrieking Legiana': 'monster_icons/MHWI-Shrieking_Legiana_Icon.png',
         'Silver Rathalos': 'monster_icons/MHWI-Silver_Rathalos_Icon.png',
+        'Teostra': 'monster_icons/MHW-Teostra_Icon.png',
         'Tigrex': 'monster_icons/MHWI-Tigrex_Icon.png',
+        'Tobi-Kadachi': 'monster_icons/MHW-Tobi-Kadachi_Icon.png',
+        'Tzitzi-Ya-Ku': 'monster_icons/MHW-Tzitzi-Ya-Ku_Icon.png',
+        'Uragaan': 'monster_icons/MHW-Uragaan_Icon.png',
+        'Vaal Hazak': 'monster_icons/MHW-Vaal_Hazak_Icon.png',
         'Velkhana': 'monster_icons/MHWI-Velkhana_Icon.png',
         'Viper Tobi-Kadachi': 'monster_icons/MHWI-Viper_Tobi-Kadachi_Icon.png',
+        'Xeno\'jiiva': 'monster_icons/MHW-Xeno\'jiiva_Icon.png',
         'Yian Garuga': 'monster_icons/MHWI-Yian_Garuga_Icon.png',
-        'Zinogre': 'monster_icons/MHWI-Zinogre_Icon.png'
+        'Zinogre': 'monster_icons/MHWI-Zinogre_Icon.png',
+        'Zorah Magdaros': 'monster_icons/MHW-Zorah_Magdaros_Icon.png'
     });
     vm.fonts = Object.freeze({
         "Heebo": 'heebo',
@@ -117,7 +117,7 @@ angular.module('app', [])
     });
 
     vm.selectedWeapon = _.keys(vm.weapons)[0];
-    vm.selectedMonster = _.keys(vm.monsters)[10];
+    vm.selectedMonster = _.assign({}, _.times(5, _.constant(_.keys(vm.monsters)[10])));
     vm.selectedRunCategory = '2';
     vm.selectedBgType = '1';
     vm.selectedTimeColorType = '1';
@@ -138,6 +138,9 @@ angular.module('app', [])
     vm.clawColor2 = "#8A99A8";
     vm.outlineCustomColor = "#FFF";
 
+    vm.numMonsters = 1;
+    vm.numAdditionalWeapons = 0;
+
     initializeHuebee('bgColor', '.bg-color-input');
     initializeHuebee('bgColor2', '.bg-color-input2');
     initializeHuebee('timeColor', '.time-color-input');
@@ -150,7 +153,7 @@ angular.module('app', [])
     var huebO = new Huebee('.outline-color-input');
     huebO.on('change', function(color, hue, sat, lum) {
         vm.outlineCustomColor = color;
-        document.querySelector(".monster-image").style.cssText = "--stroke-color:" + vm.outlineCustomColor;
+        vm.outlineChange();
         $scope.$digest();
     });
 
@@ -166,6 +169,19 @@ angular.module('app', [])
         });
         vm[selected] = _.keys(optionList)[Math.min(_.keys(optionList).length - 1, curIndex + 1)];
     };
+
+    vm.prev2 = function(selected, key, optionList) {
+        var curIndex = _.findIndex(_.keys(optionList), function(o) {
+            return o == vm[selected][key];
+        });
+        vm[selected][key] = _.keys(optionList)[Math.max(0, curIndex - 1)];
+    }
+    vm.next2 = function(selected, key, optionList) {
+        var curIndex = _.findIndex(_.keys(optionList), function(o) {
+            return o == vm[selected][key];
+        });
+        vm[selected][key] = _.keys(optionList)[Math.min(_.keys(optionList).length - 1, curIndex + 1)];
+    }
 
     vm.getBgStyle = function() {
         if (vm.selectedBgType == '0') {
@@ -189,23 +205,30 @@ angular.module('app', [])
     }
 
     vm.outlineChange = function() {
+        var cssText = "";
         if (vm.selectedOutlineType == '0') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: initial";
+            cssText = "--stroke-color: initial";
         }
         else if (vm.selectedOutlineType == '1') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: #fff";
+            cssText = "--stroke-color: #fff";
         }
         else if (vm.selectedOutlineType == '2') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: #33CC66";
+            cssText = "--stroke-color: #33CC66";
         }
         else if (vm.selectedOutlineType == '3') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: #CC00FF";
+            cssText = "--stroke-color: #CC00FF";
         }
         else if (vm.selectedOutlineType == '4') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: #FF6633";
+            cssText = "--stroke-color: #FF6633";
         }
         else if (vm.selectedOutlineType == '5') {
-            document.querySelector(".monster-image").style.cssText = "--stroke-color: " + vm.outlineCustomColor;
+            cssText = "--stroke-color: " + vm.outlineCustomColor;
+        }
+
+        if (cssText) {
+            _.each(document.getElementsByClassName('monster-image'), function(e) {
+                e.style.cssText = cssText;
+            });
         }
     }
 
@@ -214,6 +237,16 @@ angular.module('app', [])
         .then(function(blob) {
             window.saveAs(blob, 'thumbnail.png');
         });
+    }
+
+    vm.inc = function(varName, limit) {
+        if (vm[varName] < limit)
+            vm[varName]++;
+    }
+
+    vm.dec = function(varName, limit) {
+        if (vm[varName] > limit)
+            vm[varName]--;
     }
 
     function initializeHuebee(vmVar, inputClass) {
